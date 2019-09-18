c3441546db5cd95827b8dbae301b9ebc7c487ee7
Nimengli <niml1220@163.com>
2019-09-18 02:08:51 -0700

    nimengli:url configuration

--- Mainevent/urls.py
+++ Mainevent/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.urls import path
 from . import views
 
@@ -6,3 +7,15 @@ urlpatterns = [
     path('test/', views.Test.as_view()),
 
 ]
+=======
+from django.urls import path
+from . import views,datain,show_task
+
+
+urlpatterns = [
+    path('test/', views.Test.as_view()),
+    path('add/',datain.add_by_input),
+    path('push/',datain.add_from_push),
+    path('show/',show_task.show),
+]
+>>>>>>> nimengli:url configuration
