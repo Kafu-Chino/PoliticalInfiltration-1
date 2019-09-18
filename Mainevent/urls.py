49975a3d8d7f10be67f79e1f9ce59ef2420a0aa3
NiMengli <54738895+NiMengli@users.noreply.github.com>
2019-09-18 04:38:06 -0700

    Update urls.py

--- Mainevent/urls.py
+++ Mainevent/urls.py
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-from django.urls import path
-from . import views
 
-
-urlpatterns = [
-    path('test/', views.Test.as_view()),
-
-<<<<<<< HEAD
-]
-=======
 from django.urls import path
 from . import views,datain,show_task
 
@@ -18,7 +8,5 @@ urlpatterns = [
     path('add/',datain.add_by_input),
     path('push/',datain.add_from_push),
     path('show/',show_task.show),
-=======
->>>>>>> 1fbf181343fdeacfa4aaef5f2c9e91721f481a1b
 ]
 >>>>>>> nimengli:url configuration
