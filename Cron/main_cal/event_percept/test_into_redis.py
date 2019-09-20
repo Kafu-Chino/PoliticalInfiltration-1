3b9e6fcb411a977b5692859e521b0e3b93ef0915
Ymm0008 <being0008@163.com>
2019-09-20 15:21:11 +0800

    test_commit

--- Cron/main_cal/event_percept/test_into_redis.py
+++ Cron/main_cal/event_percept/test_into_redis.py
@@ -1,15 +1,23 @@
 import sys
+import os
+import django
 sys.path.append('../../../')
 
-from Config.base import es, redis_ep
+os.environ['DJANGO_SETTINGS_MODULE'] = 'PoliticalInfiltration.settings'
+django.setup()  # 启动django
+
+from Mainevent.models import *
+
+# from Config.base import es, redis_ep
 
 def test_push_redis():
 	# dic = {k:500000-k for k in range(500000)}
 	# l = [i for i in range(100)]
 	# print('over')
 	# print(redis_ep.mget(l))
-	redis_ep.delete(*redis_ep.keys())
+	# redis_ep.delete(*redis_ep.keys())
 	# print(redis_ep.keys())
+	print("nothing happened.")
 
 if __name__ == '__main__':
 	test_push_redis()
