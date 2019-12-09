73a82e72e3f509e68f1e9819c5004b0d1c8dc77e
lwenj2012 <lwenj2012@163.com>
2019-12-09 20:00:41 +0800

    李文娟20191209：修改计算框架相关代码、模型迁移

--- Cron/profile_cal/user_msg_type.py
+++ Cron/profile_cal/user_msg_type.py
@@ -1,3 +1,46 @@
 ### 在这里将get_aggs函数进行拆分，这里只放活动信息统计的部分，该文件不会直接执行，会在profile_cal_main.py里统一调用进行计算。
 
 ### 标记好这个统计函数的输入与输出，以便于代码查看
+
+
+import sys
+import os
+import time
+import datetime
+from pandas import DataFrame
+
+sys.path.append("../../")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'PoliticalInfiltration.settings'
+
+
+# 获取单个用户的活动特征，输入数据格式为{uid1:[{},{}],uid2:[{},{}]},返回格式{timestamp_uid:{},timestamp_uid2:{}}
+def get_msg_type_aggs(data_dict):
+    user_behavior_dict = {}
+    for uid in data_dict:
+        mid_dict_list = data_dict[uid]
+        df = DataFrame(mid_dict_list)
+        behavior_dict = get_msg_aggs(df)
+        behavior_dict["timestamp"] = int(time.time())
+        behavior_dict["uid"] = uid
+        behavior_dict["store_date"] = datetime.date.today()
+        user_behavior_dict["%s_%s" % (str(behavior_dict["timestamp"]), uid)] = behavior_dict
+
+    return user_behavior_dict
+
+
+# 获取单个用户的活动特征，输入数据为pandas的df
+def get_msg_aggs(df):
+    behavior_dict = {}
+    res_dict = df.groupby(df["message_type"]).size().to_dict()
+    for k, v in res_dict.items():
+        if k == 1:
+            behavior_dict["originalnum"] = v
+        elif k == 2:
+            behavior_dict["commentnum"] = v
+        elif k == 3:
+            behavior_dict["retweetnum"] = v
+    if len(behavior_dict) != 3:
+        for k in ["originalnum", "commentnum", "retweetnum"]:
+            if k not in behavior_dict:
+                behavior_dict[k] = 0
+    return behavior_dict
--- Userprofile/models.py
+++ Userprofile/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserActivity(models.Model):
+    ua_id = models.CharField(max_length=50, primary_key=True)
+    uid = models.CharField(max_length=30, blank=True, null=True)
+    statusnum = models.IntegerField(blank=True, null=True)
+    timestamp = models.BigIntegerField(blank=True, null=True)
+    send_ip = models.CharField(max_length=30, blank=True, null=True)
+    geo = models.CharField(max_length=50, blank=True, null=True)
+    store_date = models.DateField(blank=True, null=True)
+
+    class Meta():
+        db_table = 'UserActivity'
+
+
+class UserBehavior(models.Model):
+    ub_id = models.CharField(max_length=50, primary_key=True)
+    uid = models.CharField(max_length=30, blank=True, null=True)
+    originalnum = models.IntegerField(blank=True, null=True)
+    commentnum = models.IntegerField(blank=True, null=True)
+    retweetnum = models.IntegerField(blank=True, null=True)
+    timestamp = models.BigIntegerField(blank=True, null=True)
+    store_date = models.DateField(blank=True, null=True)
+
+    class Meta():
+        db_table = 'UserBehavior'
