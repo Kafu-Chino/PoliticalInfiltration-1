28cc8723f3159a217ee5ab6df995ad18bb1fda51
Kafu-Chino <huzf1218@gmail.com>
2020-02-22 15:36:02 +0800

    户中方-事件修改

--- Cron/tasks/event_add_task.py
+++ Cron/tasks/event_add_task.py
@@ -6,11 +6,11 @@ from Cron.event_cal.event_cal_main import event_cal_main
 
 
 # 更新事件库计算状态
-def update_cal_status(eid, cal_status):
+def update_cal_status(eid_dict, cal_status):
     cursor = pi_cur()
     sql = "UPDATE Event SET cal_status = %s WHERE e_id = %s"
 
-    params = [(cal_status, eid)]
+    params = [(cal_status, item['e_id']) for item in eid_dict]
     n = cursor.executemany(sql, params)
     conn.commit()
 
@@ -30,6 +30,9 @@ def event_add():
     # 获取新添加的事件
     eid_dic = get_edic_add()
 
+    # 更新为“计算中”
+    update_cal_status(eid_dic, 1)
+
     # 分事件进行计算
     print("需要计算事件 {} 个。".format(len(eid_dic)))
     for e_item in eid_dic:
@@ -37,9 +40,6 @@ def event_add():
         e_name = e_item['event_name']
         start_date = e_item['begin_date']
         end_date = e_item['end_date']
-
-        # 更新为“计算中”
-        update_cal_status(e_id, 1)
 
         # 事件计算
         event_cal_main(e_item, 0, start_date, end_date)
