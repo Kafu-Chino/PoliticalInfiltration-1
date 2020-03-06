9f4d4e54083c97b2727c677c4c2ba17bdd2c9a67
liyinghan-git <liyinghan@buaa.edu.cn>
2020-03-06 16:24:13 +0800

    lwj-20200306:新增用户更改信息，修改查询用户功能

--- Usermanager/urls.py
+++ Usermanager/urls.py
@@ -2,12 +2,12 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('test/', views.Test.as_view()),
-    path('user_info/', views.UserInfo.as_view()),
+    path('user_info/', views.User_Info.as_view()),
     path('user_login/', views.User_Login.as_view()),
     path('user_logout/', views.User_Logout.as_view()),
     path('user_add/', views.User_Add.as_view()),
     path('user_delete/', views.User_Delete.as_view()),
+    path('user_modify/', views.User_Modify.as_view()),
 ]
--- Usermanager/views.py
+++ Usermanager/views.py
@@ -15,9 +15,11 @@ class Test(APIView):
 
     def get(self, request):
         """GET方法的功能说明写在这里"""
-        username = "Admin"
-        password = hash_code("Admin123")
-        role = 1
+        # username = "Admin"
+        username = "Superuser1"
+        # password = hash_code("Admin123")
+        password = hash_code("Superuser1")
+        role = 2
         if not User.objects.filter(username=username):
             User.objects.create(username=username, password=password, role=role)
             print("%s created success" % username)
@@ -40,21 +42,26 @@ class Test(APIView):
         return HttpResponse('这是测试的DELETE方法')
 
 
-class UserInfo(APIView):
-    """展示所有用户信息接口"""
+class User_Info(APIView):
+    """查询符合条件用户信息接口"""
 
     def get(self, request):
         """
-        判断当前登录用户是否是权限为1的超级用户，是则查询User数据库，返回所有用户信息，用户名和权限;否则，返回提示无权限
+        判断当前登录用户是否是权限为1或权限为2的非普通用户，是则查询User数据库，返回查询用户信息（如未设查询条件则查询所有），用户名和权限;否则，返回提示无权限
         格式: {"status":1,"result":[{"username": username, "role": role},{},...]};
+              {"status":0,"message":"请先登录！"};
               {"status":0,"result":"暂无权限查看！"}
         """
         res_dict = {}
         try:
-            if request.session['role'] == 1:
-                result = User.objects.all().values("username", "role")
+            if request.session['role'] == 1 or request.session['role'] == 2:
+                keyword = request.GET.get('keyword')
+                if not keyword:
+                    result = User.objects.all().values("username", "role")
+                else:
+                    result = User.objects.filter(username__contains=keyword).values("username", "role")
                 res_dict["status"] = 1
-                res_dict["result"] = list(result)
+                res_dict["result"] = list(result) if len(list(result)) > 0 else "无匹配结果"
             else:
                 res_dict["status"] = 0
                 res_dict["result"] = "暂无权限查看！"
@@ -103,7 +110,7 @@ class User_Logout(APIView):
         """
         if request.session.get("is_login", None):
             request.session.flush()
-        return redirect('/Usermanager/user_login')
+        return redirect('/Usermanager/user_login/')
 
 
 class User_Add(APIView):
@@ -111,14 +118,15 @@ class User_Add(APIView):
 
     def post(self, request):
         """
-        判断当前登录用户是否是权限为1的超级用户，是则添加用户至数据库，返回添加成功;否则，返回提示无权限
+        判断当前登录用户是否是权限为1或权限为2的非普通用户，是则添加用户至数据库，返回添加成功;否则，返回提示无权限
         格式: {"status":1,"message":"添加成功"};
               {"status":0,"message":"暂无操作权限！"};
+              {"status":0,"message":"请先登录！"};
               {"status":0,"message":"用户已存在！"}
         """
         res_dict = {}
         try:
-            if request.session['role'] == 1:
+            if request.session['role'] == 1 or request.session['role'] == 2:
                 username = request.POST.get('username')
                 if not User.objects.filter(username=username).first():
                     password = request.POST.get('password')
@@ -143,18 +151,60 @@ class User_Delete(APIView):
 
     def get(self, request):
         """
-        判断当前登录用户是否是权限为1的超级用户，是则获取username,查询是否存在，执行删库操作，返回删除成功;
+        判断当前登录用户是否是权限为1或权限为2的非普通用户，是则获取username,查询是否存在，执行删库操作，返回删除成功;
         否则，返回提示无权限
         格式: {"status":1,"message":"删除成功"};
+              {"status":0,"message":"请先登录！"};
               {"status":0,"message":"暂无操作权限！"}
         """
         res_dict = {}
         username = request.GET.get('username')
         try:
-            if request.session['role'] == 1:
+            if request.session['role'] == 1 or request.session['role'] == 2:
                 User.objects.filter(username=username).delete()
                 res_dict["status"] = 1
                 res_dict["message"] = "删除成功！"
+            else:
+                res_dict["status"] = 0
+                res_dict["message"] = "暂无操作权限！"
+        except:
+            res_dict["status"] = 0
+            res_dict["message"] = "请先登录！"
+        return JsonResponse(res_dict)
+
+
+class User_Modify(APIView):
+    """更改用户信息接口"""
+
+    def post(self, request):
+        """
+        判断当前登录用户是否是权限为1或权限为2的非普通用户，是则获取username,查询是否存在，执行改库操作，返回更改信息成功;
+        否则，返回提示无权限
+        格式: {"status":1,"message":"更改信息成功"};
+              {"status":0,"message":"用户名已存在！"};
+              {"status":0,"message":"该用户不存在！"};
+              {"status":0,"message":"请先登录！"};
+              {"status":0,"message":"暂无操作权限！"}
+        """
+        res_dict = {}
+        username = request.POST.get('username')
+        try:
+            if request.session['role'] == 1 or request.session['role'] == 2:
+                if User.objects.filter(username=username).first():
+                    new_username = request.POST.get('new_username')
+                    if not User.objects.filter(username=new_username).first():
+                        new_role = request.POST.get('new_role')
+                        new_pwd = request.POST.get('new_pwd')
+                        User.objects.filter(username=username).update(username=new_username,
+                                                                      password=hash_code(new_pwd), role=new_role)
+                        res_dict["status"] = 1
+                        res_dict["message"] = "修改信息成功！"
+                    else:
+                        res_dict["status"] = 0
+                        res_dict["message"] = "用户名已存在！"
+                else:
+                    res_dict["status"] = 0
+                    res_dict["message"] = "该用户不存在！"
             else:
                 res_dict["status"] = 0
                 res_dict["message"] = "暂无操作权限！"
