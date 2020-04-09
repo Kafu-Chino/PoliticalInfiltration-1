0f00f6e6900a3cd3aa007e95079d10b522f6cc6f
Kafu-Chino <huzf1218@gmail.com>
2020-04-09 10:48:57 +0800

    户中方-事件参数

--- Config/db_utils.py
+++ Config/db_utils.py
@@ -50,7 +50,7 @@ def get_global_para(para_name):
 
 def get_event_para(e_id, para_name):
     cursor = pi_cur()
-    sql = "select prototype from EventParameter where e_id = '{}' and p_name = '{}'".format(e_id, para_name)
+    sql = "select p_value from EventParameter where e_id = '{}' and p_name = '{}'".format(e_id, para_name)
     cursor.execute(sql)
     result = cursor.fetchone()
     return float(result["p_value"])
