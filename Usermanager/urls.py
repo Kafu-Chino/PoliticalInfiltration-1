e347c10b6be90ae9085e0be62ec07d2c77f8e6f1
liyinghan-git <liyinghan@buaa.edu.cn>
2020-04-26 19:39:56 +0800

    lwj系统管理

--- Usermanager/urls.py
+++ Usermanager/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('test/', views.Test.as_view()),
     path('user_info/', views.User_Info.as_view()),
     path('user_login/', views.User_Login.as_view()),
     path('user_logout/', views.User_Logout.as_view()),
     path('user_add/', views.User_Add.as_view()),
     path('user_delete/', views.User_Delete.as_view()),
     path('user_modify/', views.User_Modify.as_view()),
+    path('user_status/', views.User_Status.as_view()),
 ]
